6d5100fdeb68a36d6b44d1df01560dd40af33e75
Anastasiia Sotnykova <nastiksot02@gmail.com>
2023-01-29 05:14:55 +0200

    feat: added plant configs

---
+++ Assets/CodeBase/Models/PlantType.cs
@@ -0,0 +1,16 @@
+﻿public enum PlantType
+{
+    None = -1,
+    Apple = 0,
+    Beetroot = 1,
+    Bonsai = 2,
+    Cactus = 3,
+    Spruce = 4,
+    Carrot = 5,
+    Orange = 6,
+    Pine = 7,
+    Plum = 8,
+    Pumpkin = 9,
+    Radish = 10,
+    Rose = 11,
+}
